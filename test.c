85135b1552bd746502e639eedd31ce8c279f9972
Igor V. Sementsov <kuzmich@macbook.kuzm>
2024-11-12 17:45:21 +1000

    fix

---
+++ test.c
@@ -0,0 +1,112 @@
+#include "config.h"
+#include <stdio.h>
+#ifdef MAKE_TEST
+#include "libtg.h"
+#include "mtx/include/api.h"
+#include "mtx/include/buf.h"
+#include "mtx/include/types.h"
+#include "tl/deserialize.h"
+#include "tl/id.h"
+#include <stdbool.h>
+#include <string.h>
+#include "tl/alloc.h"
+#include "tl/libtl.h"
+#include "tl/names.h"
+
+#include "api_id.h"
+
+
+char * callback(
+			void *userdata,
+			TG_AUTH auth,
+			const tl_t *tl)
+{
+	switch (auth) {
+		case TG_AUTH_PHONE_NUMBER:
+			{
+				char phone[32];
+				printf("enter phone number (+7XXXXXXXXXX): \n");
+				scanf("%s", phone);
+				return strdup(phone);
+			}
+			break;
+		case TG_AUTH_SENDCODE:
+			{
+				int code;
+				printf("enter code: \n");
+				scanf("%d", &code);
+				printf("code: %d\n", code);
+				char phone_code[32];
+				sprintf(phone_code, "%d", code);
+				return strdup(phone_code);
+			}
+			break;
+		case TG_AUTH_PASSWORD_NEEDED:
+			{
+				char password[64];
+				printf("enter password: \n");
+				scanf("%s", password);
+				printf("password: %s\n", password);
+				return strdup(password);
+			}
+			break;
+		case TG_AUTH_SUCCESS:
+			{
+				printf("Connected as ");
+				tl_user_t *user = (tl_user_t *)tl;
+				printf("%s (%s)!\n", 
+						string_from_buf(user->username_), 
+						string_from_buf(user->phone_));
+			}
+			break;
+		case TG_AUTH_ERROR:
+			{
+				printf("error: ");
+				if (tl && tl->_id == id_bad_msg_notification){
+					printf("bad_msg_notification: %d\n", 
+							((tl_bad_msg_notification_t *)tl)->error_code_);
+				}
+				else {
+					printf("unknown...\n");
+				}
+			}
+			break;
+
+		default:
+			break;
+	}
+
+	return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+	int apiId = 0;
+	char *apiHash = NULL;
+
+	SETUP_API_ID(apiId)
+	SETUP_API_HASH(apiHash)
+	
+	tg_t *tg = tg_new(
+			"test.db", 
+			apiId, 
+			apiHash);
+	
+	tg_connect(tg, NULL, callback);
+
+	tg_close(tg);
+	return 0;
+}
+#else
+int main(int argc, char *argv[])
+{
+	puts(
+		"Add a file called \"ipi_id.h\"\n"
+		"and place this code in that:\n\n"
+		"\t#define SETUP_API_ID(apiId) apiId = YOUR_API_ID;\n"
+		"\t#define SETUP_API_HASH(apiHash) apiHash = YOUR_API_HASH;\n"
+		);
+
+	return 0;
+}
+#endif
