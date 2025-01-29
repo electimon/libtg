2e19732e23e1e5888c49593b3a53187596097712
Igor V. Sementsov <kuzmich@macbook.kuzm>
2025-01-29 12:22:46 +1000

    fix

--- tg/ack.c
+++ tg/ack.c
@@ -28,34 +28,33 @@ void tg_add_msgid(tg_t *tg, uint64_t msgid){
 buf_t tg_ack(tg_t *tg)
 {
 	ON_LOG(tg, "%s", __func__);
-	buf_t buf = buf_new();
+	buf_t ack = buf_new();
 	
 	// send ACK
 	int err = pthread_mutex_lock(&tg->msgidsm);
 	if (err){
 		ON_ERR(tg, "%s: can't lock mutex: %d", __func__, err);
-		return buf;
+		return ack;
 	}
 
 	int i, len = arrlen(tg->msgids);
 	if (len < 1){
 		// no messages to acknolage
 		pthread_mutex_unlock(&tg->msgidsm);
-		return buf;
+		return ack;
 	}
 
 	for (i = 0; i < len; ++i) {
 		ON_ERR(tg, "ACK: "_LD_"", tg->msgids[i]);
 	}
 
-	buf_t ack = tl_msgs_ack(
+	ack = tl_msgs_ack(
 			tg->msgids, len);
-	buf_free(ack);
 
 	// free msgids
 	arrfree(tg->msgids);
 	tg->msgids = NULL;
 	pthread_mutex_unlock(&tg->msgidsm);
 
-	return buf;
+	return ack;
 }
